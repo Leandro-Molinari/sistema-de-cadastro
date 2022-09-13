a17f1ea68af75aaac17ad8952da97491fa48451f
Leandro-Molinari <113352311+Leandro-Molinari@users.noreply.github.com>
2022-09-13 13:25:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação esté se conectando ao banco de dados")
+console.log("Linha para verificar com pull no arquivo local")
 
