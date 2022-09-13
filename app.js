1dc0e3f7ddaff4216c740ded9c5e25b387ca773b
Leandro-Molinari <leandro.molinari@brxretail.com>
2022-09-13 16:01:35 -0300

    Corrigindo o app js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação esté se conectando ao banco de dados")
 console.log("Linha para verificar com pull no arquivo local")
+console.log("Incluindo essa linha só pra ficar com duas atualizações pendentes, como no vídeo do curso")
+
 
